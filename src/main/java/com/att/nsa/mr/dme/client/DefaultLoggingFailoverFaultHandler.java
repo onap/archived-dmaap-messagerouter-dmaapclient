7f3c4a2a4d193f88ba7b02ebca1e28de5fb1ba27
rama-huawei <rama.subba.reddy.s@huawei.com>
2017-09-18 15:25:58 +0530

    Fix for Sonar Major issues
    DCAEGEN2-93
    
    Change-Id: Id3b2e134d4e5b8c63712071d672b6ba6e40ab6f4
    Signed-off-by: rama-huawei <rama.subba.reddy.s@huawei.com>

--- src/main/java/com/att/nsa/mr/dme/client/DefaultLoggingFailoverFaultHandler.java
+++ src/main/java/com/att/nsa/mr/dme/client/DefaultLoggingFailoverFaultHandler.java
@@ -21,30 +21,8 @@
  *******************************************************************************/
 package com.att.nsa.mr.dme.client;
 
-//package com.att.aft.dme2.api;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-//import com.att.aft.dme2.api.DME2FailoverFaultHandler;
-//import com.att.aft.dme2.api.util.DME2Constants;
-//import com.att.aft.dme2.api.util.DME2ExchangeFaultContext;
-//import com.att.aft.dme2.api.util.LogMessage;
-//import com.att.aft.dme2.api.util.LogUtil;
 public class DefaultLoggingFailoverFaultHandler /*implements DME2FailoverFaultHandler*/ {
-    /** The logger. */
-    //private static Logger logger = DME2Constants.getLogger(DefaultLoggingFailoverFaultHandler.class.getName());
-     
-//    @Override
-//    public void handleEndpointFailover(/*DME2ExchangeFaultContext context*/) {
-//       // LogUtil.INSTANCE.report(logger, Level.WARNING, LogMessage.SEP_FAILOVER, context.getService(),context.getRequestURL(),context.getRouteOffer(),context.getResponseCode(),context.getException());
-//    }
-//    @Override
-//    /**
-//     * The DME2Exchange already has a log message when the route offer is failed over. We dont need to log it again here.
-//     */
-//    public void handleRouteOfferFailover(DME2ExchangeFaultContext context) {
-//        //noop
-//         
-//    }
 }
